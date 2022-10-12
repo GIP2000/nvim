631963f354624323e4d33f7b7ecac49673f70184
Gregory Presser <gpress2222@gmail.com>
2022-10-12 12:21:47 -0400

    Moved some keybinds

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -7,10 +7,10 @@ local silent = { silent = true }
 -- This just means I can crush, with opposite hand, the 4 terminal positions
 --
 -- These functions are stored in harpoon.  A plugn that I am developing
-nnoremap("<leader>a", function() require("harpoon.mark").add_file() end, silent)
+nnoremap("<leader>j", function() require("harpoon.mark").add_file() end, silent)
 nnoremap("<leader>h", function() require("harpoon.ui").toggle_quick_menu() end, silent)
 
-nnoremap("<leader>j", function() require("harpoon.ui").nav_file(1) end, silent)
-nnoremap("<leader>k", function() require("harpoon.ui").nav_file(2) end, silent)
-nnoremap("<leader>l", function() require("harpoon.ui").nav_file(3) end, silent)
-nnoremap("<leader>;", function() require("harpoon.ui").nav_file(4) end, silent)
+nnoremap("<leader>a", function() require("harpoon.ui").nav_file(1) end, silent)
+nnoremap("<leader>s", function() require("harpoon.ui").nav_file(2) end, silent)
+nnoremap("<leader>d", function() require("harpoon.ui").nav_file(3) end, silent)
+nnoremap("<leader>f", function() require("harpoon.ui").nav_file(4) end, silent)
