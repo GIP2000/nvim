fed6d82b66d6ad45b4db51bf477f751c9764bdd2
Gregory Presser <gpress2222@gmail.com>
2022-10-18 23:33:13 -0400

    Added python lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -109,6 +109,18 @@ require'lspconfig'.cssmodules_ls.setup{}
 require("lspconfig").tsserver.setup(config())
 require("lspconfig").svelte.setup(config())
 require("lspconfig").cssls.setup(config())
+require'lspconfig'.pylsp.setup{
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          ignore = {'W391'},
+          maxLineLength = 100
+        }
+      }
+    }
+  }
+}
 require("lspconfig").prismals.setup(config())
 require("lspconfig").rust_analyzer.setup(config({
 	cmd = { "rustup", "run", "nightly", "rust-analyzer" },
