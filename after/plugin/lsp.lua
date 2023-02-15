641bc9ad28de8a05c03f7c6a977e92119e6ba2e2
Gregory Presser <gpress2222@gmail.com>
2023-02-15 16:27:57 -0500

    cleanup

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -6,28 +6,34 @@ lsp.preset('recommended')
 
 
 lsp.on_attach(function()
-            nnoremap("K", function() vim.lsp.buf.hover() end)
-			nnoremap("gd", function() vim.lsp.buf.definition() end)
-			nnoremap("<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
-			nnoremap("<leader>ld", function() vim.diagnostic.open_float() end)
-			nnoremap("[d", function() vim.diagnostic.goto_next() end)
-			nnoremap("]d", function() vim.diagnostic.goto_prev() end)
-			nnoremap("<leader>la", function() vim.lsp.buf.code_action() end)
-			nnoremap("<leader>la", function() vim.lsp.buf.code_action({
-                filter = function(code_action)
-                    if not code_action or not code_action.data then
-                        return false
-                    end
+    nnoremap("K", function() vim.lsp.buf.hover() end)
+    nnoremap("gd", function() vim.lsp.buf.definition() end)
+    nnoremap("<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
+    nnoremap("<leader>ld", function() vim.diagnostic.open_float() end)
+    nnoremap("[d", function() vim.diagnostic.goto_next() end)
+    nnoremap("]d", function() vim.diagnostic.goto_prev() end)
+    nnoremap("<leader>la", function() vim.lsp.buf.code_action() end)
+    nnoremap("<leader>la", function() vim.lsp.buf.code_action({
+            filter = function(code_action)
+                if not code_action or not code_action.data then
+                    return false
+                end
 
-                    local data = code_action.data.id
-                    return string.sub(data, #data - 1, #data) == ":0"
-                end,
-                apply = true
-            }) end)
-			nnoremap("gr", function() vim.lsp.buf.references() end)
-			nnoremap("<leader>lr", function() vim.lsp.buf.rename() end)
-			nnoremap("<leader>lh", function() vim.lsp.buf.signature_help() end)
-		end)
+                local data = code_action.data.id
+                return string.sub(data, #data - 1, #data) == ":0"
+            end,
+            apply = true
+        })
+    end)
+    nnoremap("gr", function() vim.lsp.buf.references() end)
+    nnoremap("<leader>lr", function() vim.lsp.buf.rename() end)
+    nnoremap("<leader>lh", function() vim.lsp.buf.signature_help() end)
+end)
+lsp.configure("clangd", {
+    settings = {
+        IndentWidth = 4
+    }
+});
 lsp.setup()
 --
 -- local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -36,15 +42,18 @@ lsp.setup()
 -- -- Setup nvim-cmp.
 local cmp = require("cmp")
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
 })
 
 lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
+    mapping = cmp_mappings
 })
+
+
+
 -- local lspkind = require("lspkind")
 --
 -- cmp.setup({
