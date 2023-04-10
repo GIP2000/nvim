a3b141e9f299b92c090dc7b3da1b2238387e6cde
Gregory Presser <gpress2222@gmail.com>
2023-04-10 12:55:23 +0200

    removed shortcut for split swapping

--- lua/gip/remap.lua
+++ lua/gip/remap.lua
@@ -11,14 +11,15 @@ nnoremap("<leader>c", ":q<CR>")
 -- Move line up or down
 nnoremap("<C-k>", ":m -2<CR>==")
 nnoremap("<C-j>", ":m +1<CR>==")
-inoremap("<C-j>","<Esc>:m +1<CR>==gi")
-inoremap("<C-k>","<Esc>:m -2<CR>==gi")
-vnoremap("<C-j>",":m '>+1<CR>gv=gv")
-vnoremap("<C-k>",":m '<-2<CR>gv=gv")
+inoremap("<C-j>", "<Esc>:m +1<CR>==gi")
+inoremap("<C-k>", "<Esc>:m -2<CR>==gi")
+vnoremap("<C-j>", ":m '>+1<CR>gv=gv")
+vnoremap("<C-k>", ":m '<-2<CR>gv=gv")
 
 -- Split stuff
-nnoremap("<C-h>", "<C-w>h")
-nnoremap("<C-l>", "<C-w>l")
+-- deprecated due to yabai
+-- nnoremap("<C-h>", "<C-w>h")
+-- nnoremap("<C-l>", "<C-w>l")
 nnoremap("<leader>v", "<C-w>v")
 
 nnoremap("<leader>e", "<cmd>Ex<CR>")
