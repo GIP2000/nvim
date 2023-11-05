7cc60ed1c9d2103ac0cdf4370e8995a56e000fab
Gregory Presser <gpress2222@gmail.com>
2023-11-05 11:14:59 -0800

    remove everything

--- after/plugin/color.lua
+++
@@ -1,4 +0,0 @@
-vim.opt.background = "dark"
--- vim.cmd("colorscheme gruvbox")
-vim.cmd("colorscheme tokyonight-moon")
--- vim.cmd("colorscheme onedark")
--- after/plugin/oil.lua
+++
@@ -1,6 +0,0 @@
-local oil = require("oil")
-local Remap = require("gip.keymap")
-oil.setup()
-local nnoremap = Remap.nnoremap
-
-nnoremap("<leader>e", "<cmd>Oil<CR>")
