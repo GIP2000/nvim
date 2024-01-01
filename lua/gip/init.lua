22f04d8b1e1de2d45fba50c5f86b4ab3251f3e80
Gregory Presser <gpress2222@gmail.com>
2024-01-01 09:48:47 -0800

    updated deps and added db UI

--- lua/gip/init.lua
+++ lua/gip/init.lua
@@ -1,30 +1,29 @@
 require("gip.oil")
-
 local augroup = vim.api.nvim_create_augroup
-GIPGroup = augroup('GIP', {})
+GIPGroup = augroup("GIP", {})
 
 local autocmd = vim.api.nvim_create_autocmd
-local yank_group = augroup('HighlightYank', {})
+local yank_group = augroup("HighlightYank", {})
 
 function R(name)
-    require("plenary.reload").reload_module(name)
+	require("plenary.reload").reload_module(name)
 end
 
-autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
+autocmd("TextYankPost", {
+	group = yank_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
 })
 
 autocmd({ "BufWritePre" }, {
-    group = GIPGroup,
-    pattern = "*",
-    command = "%s/\\s\\+$//e",
+	group = GIPGroup,
+	pattern = "*",
+	command = "%s/\\s\\+$//e",
 })
 
 -- autocmd({ "BufWritePre" }, {
--- lua/gip/oil.lua
+++ lua/gip/oil.lua
@@ -1,6 +1,4 @@
-local oil = require("oil")
 local Remap = require("gip.keymap")
-oil.setup()
 local nnoremap = Remap.nnoremap
 
 nnoremap("<leader>e", "<cmd>Oil<CR>")
