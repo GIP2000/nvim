031842447bc0ae9521a2e7870b4c2f474e81cbb2
Gregory Presser <gpress2222@gmail.com>
2024-01-08 09:23:36 -0800

    got rid of the luaSnip errors

--- init.lua
+++ init.lua
@@ -52,11 +52,11 @@ require("lazy").setup({
 			{ "hrsh7th/nvim-cmp" },
 			-- { "hrsh7th/cmp-buffer" },
 			{ "hrsh7th/cmp-path" },
-			-- { "saadparwaiz1/cmp_luasnip" },
+			{ "saadparwaiz1/cmp_luasnip" },
 			-- { "hrsh7th/cmp-nvim-lua" },
 
 			-- Snippets
-			-- { "L3MON4D3/LuaSnip" },
+			{ "L3MON4D3/LuaSnip" },
 		},
 		config = function()
 			require("gip.lsp")
